a72e3c79bbeb9dbacb8324fac964cfd7b7a0f152
Sriram Viswanathan <sriramvi@andrew.cmu.edu>
2023-04-07 20:56:54 -0400

    Working condition for backend servers

--- ad-server/index.ts
+++ ad-server/index.ts
@@ -3,13 +3,53 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+const bodyParser = require('body-parser');
+const jsonBodyParser = bodyParser.json!!();
+const formBodyParser = bodyParser.urlencoded!!({extended: true});
+
 const app: Express = express();
+app.use(jsonBodyParser, formBodyParser);
 const port = process.env.PORT;
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Express + TypeScript Server');
-});
+import type { Bucket } from '@blyss/sdk';
+const blyss = require('@blyss/sdk/node');
+process.removeAllListeners('warning');
 
-app.listen(port, () => {
-    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
-});
+async function main() {
+    const bucket: Bucket = await blyss.Bucket.initializeLocal(
+        'http://localhost:8008'
+    );
+
+    // Write some data to it
+    await bucket.write({
+        Health: [],
+        Electronics: [],
+        Insurance: []
+    });
+
+    app.get('/clear', async (req: Request, res: Response) => {
+        await bucket.destroyEntireBucket();
+        res.json()
+    });
+
+    app.post('/ad', async (req: Request, res: Response) => {
+        const adCategory = req.query.category as string;
+        const image = req.body.img;
+        if (adCategory && image) {
+            const result = (await bucket.privateRead(adCategory)) as string[] || [];
+            result.push(image);
+            await bucket.write({
+                [adCategory]: result
+            })
+            res.json({result})
+        }
+        res.status(400);
+    });
+
+    app.listen(port, () => {
+        console.log(`⚡️[server]: Ad Server is running at http://localhost:${port}`);
+    });
+}
+
+main();
+
--- pir/index.ts
+++ pir/index.ts
@@ -3,13 +3,36 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
+const bodyParser = require('body-parser').json();
+const sample = require('lodash/sample');
+
 const app: Express = express();
+app.use(bodyParser);
 const port = process.env.PORT;
 
-app.get('/', (req: Request, res: Response) => {
-    res.send('Express + TypeScript Server');
-});
+import type { Bucket } from '@blyss/sdk';
+const blyss = require('@blyss/sdk/node');
+process.removeAllListeners('warning');
 
-app.listen(port, () => {
-    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
-});
+async function main() {
+    const bucket: Bucket = await blyss.Bucket.initializeLocal(
+        'http://localhost:8008'
+    );
+
+    app.get('/', (req: Request, res: Response) => {
+        res.send('Express + TypeScript Server');
+    });
+
+    app.get('/ad', async (req: Request, res: Response) => {
+        const adCategory = req.query.category as string;
+        const result = await bucket.privateRead(adCategory) as String[];
+        res.json({result: sample(result)})
+    });
+
+    app.listen(port, () => {
+        console.log(`⚡️[server]: PIR Server is running at http://localhost:${port}`);
+    });
+}
+
+main();
+
